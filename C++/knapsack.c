b12fb681d73d341ec473e97cfe55e60b4501d24f
abhii0 <78195283+abhii0@users.noreply.github.com>
2022-07-23 06:13:30 +0530

    Add files via upload

---
+++ C++/knapsack.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int max(int a, int b) {
+   if(a>b){
+      return a;
+   } else {
+      return b;
+   }
+}
+int knapsack(int W, int wt[], int val[], int n) {
+   int i, w;
+   int knap[n+1][W+1];
+   for (i = 0; i <= n; i++) {
+      for (w = 0; w <= W; w++) {
+         if (i==0 || w==0)
+            knap[i][w] = 0;
+         else if (wt[i-1] <= w)
+            knap[i][w] = max(val[i-1] + knap[i-1][w-wt[i-1]], knap[i-1][w]);
+         else
+            knap[i][w] = knap[i-1][w];
+      }
+   }
+   return knap[n][W];
+}
+int main() {
+   int val[1000];
+   int wt[1000];
+   int i,j,n,W;
+   printf("Enter weight : ");
+   scanf("%d",&W);
+   printf("Enter total number of objects : ");
+   scanf("%d",&n);
+   printf("Enter the profit values : ");
+   for(i=0;i<n;i++)
+   {
+      scanf("%d",&val[i]); 
+   }
+
+   printf("Enter the weight values : ");
+   for(j=0;j<n;j++)
+   {
+       scanf("%d",&wt[j]);
+   }
+    
+   printf("The solution is : %d", knapsack(W, wt, val, n));
+   return 0;
+}
